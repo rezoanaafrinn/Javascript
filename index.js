394b998a1fe2bc0858216fcffab37d74a2dd3d75
rezoanaafrinn <rezoanaafrinn@gmail.com>
2025-04-20 16:34:18 +0600

    personal expense tracker

---
+++ index.js
@@ -0,0 +1,10 @@
+// This is a simple JavaScript program that prints the name, income, expense, and savings of a person.
+console.log("Rezoana Afrin");
+console.log(7000);
+console.log(5000);
+
+const name = "Rezoana Afrin";
+let income = 7000;
+let expense = 5000;
+let savings = income - expense;
+document.write("Name: " + name + ", Income: " + income + ", Expense: " + expense + ", Savings: " + savings);
